7a2fcd0426ac01d333c987c4ab67a9ff297ad430
moshe552 <mosdvo22@gmail.com>
2023-10-26 15:50:24 +0300

    Anonymous function added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,20 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
-function createContact(contact) {
-  return(
-    <Card
-      key={contact.id}
-      name={contact.name}
-      img={contact.img}
-      img_alt={contact.img_alt}
-      tel={contact.tel}
-      email={contact.email}
-    />
-  )
-}
-
 function App() {
   return (<div>
     <h1 className="heading">My Contacts</h1>
-    {contacts.map(createContact)}
+    {contacts.map(contact => (
+      <Card
+        key={contact.id}
+        name={contact.name}
+        img={contact.img}
+        img_alt={contact.img_alt}
+        tel={contact.tel}
+        email={contact.email}
+      />
+    )
+    )}
   </div>
   );
 }
